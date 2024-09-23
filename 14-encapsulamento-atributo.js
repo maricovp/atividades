e6e09a157bd389c3ed7da8bc2c633a7b7c622b3b
Marcos Pereira <marcos2015vinicuis@gmail.com>
2024-09-23 10:05:00 -0400

    add exemplos

---
+++ 14-encapsulamento-atributo.js
@@ -0,0 +1,17 @@
+class personagem {
+    #nome;
+    #classe;
+    #nivel;
+    #vida;
+    #mana;
+
+}
+
+constructor(nome, classe , nivel , vida , mana)  
+{
+    this.#nome = nome;
+    this.#classe = classe;
+    this.#nivel = nivel;
+    this.#vida = vida;
+    this.#mana = mana;
+}
---
+++ 15-encapsulamento getter-setters-.js
@@ -0,0 +1,47 @@
+class personagem {
+  #nome;
+  #classe;
+  #nivel;
+  #vida;
+  #mana;
+
+
+constructor(nome, classe, nivel, vida, mana) {
+  this.#nome = nome;
+  this.#classe = classe;
+  this.#nivel = nivel;
+  this.#vida = vida;
+  this.#mana = mana;
+}
+
+get nome() {
+  return this.#nome;
+}
+set nome(nome) {
+  this.#nome = nome;
+}
+get classe() {
+  return this.#classe;
+}
+set classe(classe) {
+  this.#classe = classe;
+}
+get nivel() {
+  return this.#nivel;
+}
+set niveç(nivel) {
+  this.#nivel = nivel;
+}
+get vida() {
+  return this.#vida;
+}
+set vida(vida) {
+  this.#vida = vida;
+}
+get mana() {
+  return this.#mana;
+}
+set mana(mana) {
+  this.#vida = mana;
+}
+}
