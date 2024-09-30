7ace9c6d359c9180ed265483f6bf395020a24aee
maricovp <marcos2015vinicuis@gmail.com>
2024-09-30 09:30:10 -0400

    adicionada classe abstract

---
+++ 18-classe-abstrata.java
@@ -0,0 +1,9 @@
+abstract class  Personagem {
+    protected String nome;
+    
+    public personagem(String nome) {
+        this.nome = nome;
+}
+public abstract void atacar();
+public abstract void defender();
+}
