329e8150b33eaaed3544789d6da95ea8e8b013d1
Maricovp <Marcos2015vinicuis@gmail.com>
2024-09-30 10:32:09 -0400

    add classe abstrata em javascript

---
+++ 18-classe-abstrata-em-javascript.js
@@ -0,0 +1,17 @@
+class Personagem {
+    constructor(nome) {
+        if (new target === Personagem) {
+        throw new Error ("Não é possivel instanciar a classe abstrata Personagem diretamente");
+        this.nome = nome;
+    }
+}
+
+atacar ( ) {
+    throw new Error ("Método atacar, deve ser implementado pela classe derivada");
+
+}
+defender ( ) {
+
+    throw new Error ("Método defender, deve ser implementado pela classe derivada");
+}
+}
